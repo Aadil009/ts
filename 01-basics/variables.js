71e6b3b65823930259c3df0095aa4c85c9bcd615
Aadil009 <aadil_sayyed@outlook.com>
2024-12-27 17:30:12 +0530

    variable

---
+++ 01-basics/variables.js
@@ -0,0 +1,4 @@
+var username = "Aadil";
+var age = 25;
+var isActive = true;
+console.log("User: ".concat(username, ", Age : ").concat(age, ", isActive:").concat(isActive));
---
+++ 01-basics/variables.ts
@@ -0,0 +1,4 @@
+let username: string = "Aadil"
+let age: number = 25
+let isActive: boolean = true;
+console.log(`User: ${username}, Age : ${age}, isActive:${isActive}`)
